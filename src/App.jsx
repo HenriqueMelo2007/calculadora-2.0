b4b16dadf5e081b16827656e9db7ed040beb05f3
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-05-25 17:19:01 -0300

    estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
+import { useContext } from "react"
 import { Btn } from "./components/button/btn"
-import { ProvedorContexto } from "./context/contexto"
+import { Contexto } from "./context/contexto"
 
 export function App() {
+
+  const { estado } = useContext(Contexto)
 
   const listaValores = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '=', '+', '-', '*', '/']
   
   return (
 
-    <ProvedorContexto>
-      <div className="calculadora">
+    <div className="calculadora">
 
-        <div className="calculos-box">
-          <p id="calculos"></p>
-        </div>
-
-        <div className="botoes">
-
-          {listaValores.map( (item) => {
-            return <Btn key={item} valor={item}></Btn>
-          } )}
-
-        </div>
+      <div className="calculos-box">
+        <p id="calculos">{estado}</p>
       </div>
-    </ProvedorContexto>
+
+      <div className="botoes">
+
+        {listaValores.map( (item) => {
+           return <Btn key={item} valor={item}></Btn>
+        } )}
+
+      </div>
+    </div>
 
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import './index.css'
 
+import { ProvedorContexto } from './context/contexto.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProvedorContexto>
+      <App />
+    </ProvedorContexto>
   </React.StrictMode>,
 )
